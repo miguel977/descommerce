b5c9d143cc98a77f54b81871016695fbc014913e
miguel977 <miguel645@gmail.com>
2023-08-04 11:15:25 -0300

    put product

--- src/main/java/com/devsuperior/dscommerce/service/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/service/ProductService.java
@@ -38,6 +38,19 @@ public class ProductService {
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		Product entity = repository.getReferenceById(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+	  private void copyDtoToEntity(ProductDTO dto, Product entity) {
+	        entity.setName(dto.getName());
+	        entity.setDescription(dto.getDescription());
+	        entity.setPrice(dto.getPrice());
+	        entity.setImgUrl(dto.getImgUrl());
+	    }
 	
-
+   
 }
